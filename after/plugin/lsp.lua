cdd3ff6471c6805b1956528e3a389bdd320bae73
Benjamin Michotte <bmichotte@gmail.com>
2023-02-27 12:32:54 +0100

    more config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,26 +1,125 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = { "eslint", "tsserver", "lua_ls", "prismals", "sqlls", "tailwindcss" }
+    ensure_installed = { "tsserver", "lua_ls", "prismals", "tailwindcss" }
 })
-
+local lspconfig = require("lspconfig")
 local null_ls = require("null-ls")
+local typescript = require("typescript")
+
+local lspsaga = require("lspsaga")
+lspsaga.setup({})
 
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
+        --null_ls.builtins.diagnostics.eslint,
+        --null_ls.builtins.completion.spell,
         require("typescript.extensions.null-ls.code-actions"),
     },
 })
 
-require("typescript").setup({
+local null_ls = require("null-ls")
+local eslint = require("eslint")
+
+--[[local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+{ command = "eslint_d",
+   filetypes = {"typescript", "typescriptreact", "javascript", "javascriptreact"}
+}]]
+
+eslint.setup({
+  bin = 'eslint_d', -- or `eslint_d`
+  code_actions = {
+    enable = true,
+    apply_on_save = {
+      enable = true,
+      types = { "directive", "problem", "suggestion", "layout" },
+    },
+    disable_rule_comment = {
+      enable = true,
+      location = "separate_line", -- or `same_line`
+    },
+  },
+  diagnostics = {
+    enable = true,
+    report_unused_disable_directives = false,
+    run_on = "type", -- or `save`
+  },
+})
+
+local keymap = vim.keymap -- for conciseness
+
+-- enable keybinds only for when lsp server available
+local on_attach = function(client, bufnr)
+  -- keybind options
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+
+  -- set keybinds
+  keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
+  keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+  keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
+  keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+  keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+  keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+  keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+  keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+  keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
+  keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+  keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
+  keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
+
+  -- typescript specific keymaps (e.g. rename file and update imports)
+  if client.name == "tsserver" then
+    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+  end
+end
+
+-- Change the Diagnostic symbols in the sign column (gutter)
+-- (not in youtube nvim video)
+local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+-- configure html server
+lspconfig["html"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure typescript server with plugin
+typescript.setup({
+  server = {
+    capabilities = capabilities,
+    on_attach = on_attach,
+  },
     disable_commands = false, -- prevent the plugin from creating Vim commands
     debug = false, -- enable debug logging for commands
     go_to_source_definition = {
         fallback = true, -- fall back to standard LSP definition on failure
     },
-    server = { -- pass options to lspconfig's setup method
-        --on_attach = ...,
-    },
 })
+
+
+-- configure css server
+lspconfig["cssls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure tailwindcss server
+lspconfig["tailwindcss"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+})
+
+-- configure emmet language server
+lspconfig["emmet_ls"].setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+})
+
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,7 +2,7 @@ require("Comment").setup()
 
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = { "bash", "css", "gitattributes", "gitignore", "gitcommit", "html", "javascript", "json", "lua", "markdown", "php", "prisma", "sql", "typescript" },
+    ensure_installed = { "bash", "css", "gitattributes", "gitignore", "gitcommit", "html", "javascript", "json", "lua", "markdown", "php", "prisma", "sql", "typescript", "markdown", "markdown_inline" },
     sync_install = true,
     auto_install = true,
     context_commentstring = {
