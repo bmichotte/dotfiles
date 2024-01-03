62cf3910db84b0c0f6d17f35bb95610da451ae9b
Benjamin Michotte <bmichotte@gmail.com>
2024-01-03 21:51:04 +0100

    change title

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -2,47 +2,20 @@ return {
     'goolord/alpha-nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     event = "VimEnter",
-    opts = function()
-        local dashboard = require("alpha.themes.dashboard")
-        local logo = [[
-                                             
-      ████ ██████           █████      ██
-     ███████████             █████ 
-     █████████ ███████████████████ ███   ███████████
-    █████████  ███    █████████████ █████ ██████████████
-   █████████ ██████████ █████████ █████ █████ ████ █████
- ███████████ ███    ███ █████████ █████ █████ ████ █████
-██████  █████████████████████ ████ █████ █████ ████ ██████
-]]
-        dashboard.section.header.val = vim.split(logo, "\n")
-        dashboard.section.buttons.val = {
-            dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-            dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-            dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-            --   dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-            dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
-            dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.startify")
+
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
         }
-        dashboard.section.header.opts.hl = "AlphaHeader"
-        dashboard.opts.layout[1].val = 6
-        return dashboard
-    end,
-    config = function(_, dashboard)
-        --require 'alpha'.setup(require 'alpha.themes.startify'.config)
-        require("alpha").setup(dashboard.opts)
-        vim.api.nvim_create_autocmd("User", {
-            callback = function()
-                local stats = require("lazy").stats()
-                local ms = math.floor(stats.startuptime * 100) / 100
-                dashboard.section.footer.val = "󱐌 Lazy-loaded "
-                    .. stats.loaded
-                    .. "/"
-                    .. stats.count
-                    .. " plugins in "
-                    .. ms
-                    .. "ms"
-                pcall(vim.cmd.AlphaRedraw)
-            end,
-        })
+        alpha.setup(dashboard.opts)
     end
 }
