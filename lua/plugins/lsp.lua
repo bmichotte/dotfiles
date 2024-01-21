07d3962d977a24f9152e5be940877e27b3eeef39
Benjamin Michotte <bmichotte@gmail.com>
2024-01-21 14:23:31 +0100

    remove lspsaga and lsp-zero for mason + vim.lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,92 +1,46 @@
 return {
     {
-        -- Improves the Neovim built-in LSP experience.
-        "glepnir/lspsaga.nvim",
-        branch = "main",
-        config = function()
-            require('lspsaga').setup({})
-        end,
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'nvim-tree/nvim-web-devicons',
-        }
-    },
-    {
         "williamboman/mason.nvim",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",
+            "neovim/nvim-lspconfig",
+        },
         lazy = false,
         config = function()
             require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        config = function()
-            require("mason-lspconfig").setup({
-                handlers = {
-                    require('lsp-zero').default_setup,
-                },
-                auto_install = true,
-            })
-        end,
-    },
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v2.x",
-        dependencies = {
-            "hrsh7th/nvim-cmp",
-            "neovim/nvim-lspconfig",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
-            local lsp = require("lsp-zero").preset({})
+            local lspconfig = require("lspconfig")
+            local util = require("lspconfig/util")
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({ buffer = bufnr })
+            local on_attach = function(client, bufnr)
                 vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
 
                 if client.server_capabilities.inlayHintProvider then
                     vim.lsp.inlay_hint.enable(bufnr, true)
                 end
 
-                if client.name == 'tsserver' then
-                    vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Show definition, references" })
-                    vim.keymap.set("n", "gD", "<Cmd>Lspsaga goto_definition<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Got to declaration" })
-                    vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "See definition and make edits in window" })
-                    vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Go to implementation" })
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "See available code actions" })
-                    vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Smart rename" })
-                    vim.keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Show  diagnostics for line" })
-                    vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Show diagnostics for cursor" })
-                    vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>",
-                        {
-                            noremap = true,
-                            silent = true,
-                            buffer = bufnr,
-                            desc =
-                            "Show documentation for what is under cursor"
-                        })
-
-                    vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Jump to previous diagnostic in buffer" })
-                    vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "Jump to next diagnostic in buffer" })
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>",
-                        { noremap = true, silent = true, buffer = bufnr, desc = "See code actions" })
-                else
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action,
-                        { noremap = true, silent = true, buffer = bufnr, desc = "See code actions" })
-                end
-                vim.keymap.set('i', '<C-h>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+                -- vim.keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", { noremap = true, silent = true, buffer = bufnr, desc = "Show definition, references" })
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Got to declaration" })
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "See definition and make edits in window" })
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Go to implementation" })
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Smart rename" })
+                vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Show  diagnostics for line" })
+                -- vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", { noremap = true, silent = true, buffer = bufnr, desc = "Show diagnostics for cursor" })
+                vim.keymap.set("n", "K", vim.lsp.buf.hover,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Show documentation for what is under cursor" })
+                vim.keymap.set("n", "[d", vim.diagnostic.goto_prev,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Jump to previous diagnostic in buffer" })
+                vim.keymap.set("n", "]d", vim.diagnostic.goto_next,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "Jump to next diagnostic in buffer" })
+                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action,
+                    { noremap = true, silent = true, buffer = bufnr, desc = "See code actions" })
+                vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help,
                     { noremap = true, silent = true, buffer = bufnr, desc = "Show signature help" })
 
                 local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
@@ -102,134 +56,109 @@ return {
                     end,
                     desc = "[lsp] format on save",
                 })
-            end)
+            end
 
-            local lspconfig = require("lspconfig")
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-            lsp.set_sign_icons({
-                error = "",
-                warn = "",
-                hint = " ",
-                info = " ",
-            })
-            --lsp.skip_server_setup({ "tsserver" })
-
-            --[[lsp.set_server_config({
-                capabilities = {
-                    textDocument = {
-                        foldingRange = {
-                            dynamicRegistration = false,
-                            lineFoldingOnly = true,
-                        },
-                    },
+            require("mason-lspconfig").setup({
+                auto_install = true,
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                            on_attach = on_attach,
+                        })
+                    end,
+                    ["tsserver"] = function()
+                        lspconfig.tsserver.setup({
+                            capabilities = capabilities,
+                            root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+                            init_options = {
+                                preferences = {
+                                    includeInlayParameterNameHints = "all",
+                                    includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                                    includeInlayFunctionParameterTypeHints = true,
+                                    includeInlayVariableTypeHints = false,
+                                    includeInlayPropertyDeclarationTypeHints = true,
+                                    includeInlayFunctionLikeReturnTypeHints = false,
+                                    includeInlayEnumMemberValueHints = false,
+                                    importModuleSpecifierPreference = 'non-relative'
+                                },
+                            },
+                            on_attach = function(client, bufnr)
+                                require("twoslash-queries").attach(client, bufnr)
+                                -- client.server_capabilities.document_formatting = false
+                                -- client.server_capabilities.document_range_formatting = false
+                                on_attach(client, bufnr)
+                            end,
+                        })
+                    end,
+                    ["lua_ls"] = function()
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    format = {
+                                        enable = true,
+                                    },
+                                    diagnostics = {
+                                        globals = { "vim" },
+                                        neededFileStatus = {
+                                            ["codestyle-check"] = "Any",
+                                        },
+                                        groupSeverity = { ["codestyle-check"] = "Warning", },
+                                    },
+                                    runtime = {
+                                        version = 'LuaJIT'
+                                    },
+                                    workspace = {
+                                        checkThirdParty = false,
+                                    },
+                                    completion = {
+                                        callSnippet = "Replace",
+                                    },
+                                },
+                            },
+                            on_attach = on_attach,
+                        })
+                    end,
                 },
-            })]]
-
-            lsp.format_on_save({
-                format_opts = {
-                    async = false,
-                    timeout_ms = 10000,
-                },
             })
 
-            lsp.format_mapping("gq", {
-                format_opts = {
-                    async = false,
-                    timeout_ms = 10000,
-                },
+            vim.diagnostic.config({
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = "",
+                        [vim.diagnostic.severity.WARN] = "",
+                        [vim.diagnostic.severity.HINT] = " ",
+                        [vim.diagnostic.severity.INFO] = " ",
+                    }
+                }
             })
 
-            lsp.setup()
+            -- format on save
+            local setup_group = vim.api.nvim_create_augroup("lsp_format_config", { clear = true })
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = setup_group,
+                desc = 'Enable format on save',
+                callback = function(event)
+                    local format_group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = true })
+                    vim.api.nvim_create_autocmd('BufWritePre', {
+                        group = format_group,
+                        buffer = event.buf,
+                        desc = "Format buffer",
+                        callback = function()
+                            vim.lsp.buf.format({
+                                async = false,
+                                timeout_ms = 10000,
+                            })
+                        end,
+                    })
 
-            lspconfig.biome.setup({
-                capabilities = capabilities
-            })
-            local util = require("lspconfig/util")
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-                root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-                init_options = {
-                    preferences = {
-                        includeInlayParameterNameHints = "all",
-                        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                        includeInlayFunctionParameterTypeHints = true,
-                        includeInlayVariableTypeHints = false,
-                        includeInlayPropertyDeclarationTypeHints = true,
-                        includeInlayFunctionLikeReturnTypeHints = false,
-                        includeInlayEnumMemberValueHints = false,
-                        importModuleSpecifierPreference = 'non-relative'
-                    },
-                },
-                on_attach = function(client, bufnr)
-                    require("twoslash-queries").attach(client, bufnr)
-                    -- client.server_capabilities.document_formatting = false
-                    -- client.server_capabilities.document_range_formatting = false
-                    -- on_attach(client, bufnr)
+                    vim.keymap.set({ 'n', 'x' }, '<leader>f',
+                        function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end,
+                        { buffer = event.buf, desc = "Format buffer" })
                 end,
             })
-            lspconfig.html.setup({
-                capabilities = capabilities
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                settings = {
-                    Lua = {
-                        format = {
-                            enable = true,
-                        },
-                        diagnostics = {
-                            globals = { "vim" },
-                            neededFileStatus = {
-                                ["codestyle-check"] = "Any",
-                            },
-                            groupSeverity = { ["codestyle-check"] = "Warning", },
-                        },
-                        workspace = {
-                            checkThirdParty = false,
-                        },
-                        completion = {
-                            callSnippet = "Replace",
-                        },
-                    },
-                },
-                on_init = function(client)
-                    local path = client.workspace_folders[1].name
-                    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-                        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                            Lua = {
-                                runtime = {
-                                    version = 'LuaJIT'
-                                },
-                                workspace = {
-                                    checkThirdParty = false,
-                                    -- library = {
-                                    --     vim.env.VIMRUNTIME,
-                                    --     -- vim.fn.stdpath("data")
-                                    -- }
-                                }
-                            }
-                        })
-
-                        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-                    end
-                    return true
-                end
-            })
-            lspconfig.prismals.setup({
-                capabilities = capabilities,
-            })
-
-            vim.cmd(
-                [[
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost *.js,*.lua,*.prisma,*.css,*.ts,*.tsx,*.json FormatWrite
-augroup END
-]],
-                true
-            )
-        end
+        end,
     },
     {
         "pmizio/typescript-tools.nvim",
