2e85bc15de9cdbe937f9fd62bc17725761faf3c4
Benjamin Michotte <bmichotte@gmail.com>
2024-01-25 17:10:51 +0100

    add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,18 @@
+return {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        signs = {
+            error = "",
+            warning = "",
+            hint = "",
+            information = "",
+            other = "",
+        },
+    },
+    keys = {
+        { "<leader>dt", function() require("trouble").toggle("workspace_diagnostics") end,               desc = "Toggle workspace diagnostics" },
+        { "<leader>dj", function() require("trouble").next({ skip_groups = true, jump = true }) end,     desc = "Next diagnostic" },
+        { "<leader>dk", function() require("trouble").previous({ skip_groups = true, jump = true }) end, desc = "Previous diagnostic" },
+    }
+}
