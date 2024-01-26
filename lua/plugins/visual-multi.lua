5b60a7cec37a1bb039a3bc4d5da6f6d6c47039a3
Benjamin Michotte <bmichotte@gmail.com>
2024-01-26 14:44:51 +0100

    fix an issue with autopairs

--- lua/plugins/visual-multi.lua
+++ lua/plugins/visual-multi.lua
@@ -5,6 +5,7 @@ return {
         vim.g.VM_maps = {
             ["Select Cursor Down"] = '<M-C-Down>',
             ["Select Cursor Up"]   = '<M-C-Up>',
+            ["I BS"]               = '', -- disable backspace mapping to avoid an error
         }
     end
 }
