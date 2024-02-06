8d7d3deb299fa928d6428c381fb73d957aa86c8d
Benjamin Michotte <bmichotte@gmail.com>
2024-02-06 09:19:12 +0100

    use codellama as model

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -17,20 +17,14 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
-
-        -- All the user commands added by the plugin
         cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
         keys = {
-            -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
             {
                 "<leader>oo",
                 ":<c-u>lua require('ollama').prompt()<cr>",
                 desc = "ollama prompt",
                 mode = { "n", "v" },
             },
-
-            -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
             {
                 "<leader>oG",
                 ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
@@ -38,10 +32,9 @@ return {
                 mode = { "n", "v" },
             },
         },
-
         ---@type Ollama.Config
         opts = {
-            -- your configuration overrides
+            model = "codellama"
         }
     },
     {
