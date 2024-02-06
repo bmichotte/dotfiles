efb3fbcb8c492633a672ccef45accc8070175452
Benjamin Michotte <bmichotte@gmail.com>
2024-02-06 08:31:04 +0100

    refactor folds

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
     {
         "williamboman/mason.nvim",
@@ -7,9 +8,33 @@ return {
             "hrsh7th/cmp-nvim-lsp",
             "neovim/nvim-lspconfig",
             "nvimdev/lspsaga.nvim",
-            "folke/neodev.nvim"
+            "folke/neodev.nvim",
+            "kevinhwang91/nvim-ufo",
+            "kevinhwang91/promise-async",
         },
+        init = function()
+            -- init fold
+            vim.o.foldcolumn = "1"
+            vim.o.foldlevel = 99
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+        end,
         lazy = false,
+        keys = {
+            -- za toggle folds
+            { 'zR', function() require('ufo').openAllFolds() end,  desc = 'Open all folds' },
+            { 'zM', function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
+            {
+                'zK',
+                function()
+                    local winid = require('ufo').peekFoldedLinesUnderCursor()
+                    if not winid then
+                        vim.lsp.buf.hover()
+                    end
+                end,
+                desc = 'Peek fold'
+            },
+        },
         config = function()
             require("mason").setup({
                 ui = {
@@ -22,6 +47,10 @@ return {
             local lspconfig = require("lspconfig")
             local util = require("lspconfig/util")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            capabilities.textDocument.foldingRange = {
+                dynamicRegistration = false,
+                lineFoldingOnly = true
+            }
 
             require('lspsaga').setup({
                 lightbulb = {
@@ -199,6 +228,39 @@ return {
                         function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end,
                         { buffer = event.buf, desc = "Format buffer" })
                 end,
+            })
+
+            require('ufo').setup({
+                provider_selector = function()
+                    return { 'lsp', 'indent' }
+                end,
+                fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+                    local newVirtText = {}
+                    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+                    local sufWidth = vim.fn.strdisplaywidth(suffix)
+                    local targetWidth = width - sufWidth
+                    local curWidth = 0
+                    for _, chunk in ipairs(virtText) do
+                        local chunkText = chunk[1]
+                        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                        if targetWidth > curWidth + chunkWidth then
+                            table.insert(newVirtText, chunk)
+                        else
+                            chunkText = truncate(chunkText, targetWidth - curWidth)
+                            local hlGroup = chunk[2]
+                            table.insert(newVirtText, { chunkText, hlGroup })
+                            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                            -- str width returned from truncate() may less than 2nd argument, need padding
+                            if curWidth + chunkWidth < targetWidth then
+                                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+                            end
+                            break
+                        end
+                        curWidth = curWidth + chunkWidth
+                    end
+                    table.insert(newVirtText, { suffix, 'MoreMsg' })
+                    return newVirtText
+                end
             })
         end,
     },
