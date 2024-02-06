51802756f2db6c8e0363c3c6e465d18d78a2d569
Benjamin Michotte <bmichotte@gmail.com>
2024-02-06 08:32:44 +0100

    merge all ai stuff in one file

---
+++ config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,55 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        event = "VimEnter",
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+        },
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        dependencies = { "copilot.lua" },
+        config = true
+    },
+    {
+        "jackMort/ChatGPT.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        opts = {
+            -- api_key_cmd = "op read op://Personal/msrznwludsumyoz7v7y5xhurom/identifiant --no-newline",
+            openai_params = {
+                model = "gpt-4-1106-preview",
+                frequency_penalty = 0,
+                presence_penalty = 0,
+                max_tokens = 300,
+                temperature = 0,
+                top_p = 1,
+                n = 1,
+            },
+            openai_edit_params = {
+                model = "gpt-4-1106-preview",
+                frequency_penalty = 0,
+                presence_penalty = 0,
+                temperature = 0,
+                top_p = 1,
+                n = 1,
+            },
+        },
+        keys = {
+            {
+                "<leader>cc",
+                function()
+                    local chatgpt = require('chatgpt')
+                    chatgpt.edit_with_instructions()
+                end,
+                mode = "v",
+                desc = "Edit current selection with ChatGPT"
+            }
+        }
+    }
+}
