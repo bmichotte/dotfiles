45bbfa663f7a99e07e5b53e6694f76f71af4dc37
Benjamin Michotte <bmichotte@gmail.com>
2024-05-21 08:35:16 +0200

    change comment plugin

--- config/nvim/lua/plugins/comment.lua
+++ config/nvim/lua/plugins/comment.lua
@@ -1,22 +1,9 @@
 ---@type LazyPlugin[]
 return {
     {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup({
-                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-            })
-        end,
-        lazy = false,
-    },
-    {
-        "JoosepAlviste/nvim-ts-context-commentstring",
-        opts = {
-            enable_autocmd = false,
-        },
-        init = function()
-            vim.g.skip_ts_context_commentstring_module = true
-        end,
+        "folke/ts-comments.nvim",
+        opts = {},
+        event = { "VeryLazy" },
     },
     {
         "folke/todo-comments.nvim",
