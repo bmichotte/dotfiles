0a18dfcd0032ffe83f5895add0a6a57b063d2ad5
Benjamin Michotte <bmichotte@gmail.com>
2024-06-19 09:56:33 +0200

    sort json with jq

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -3,6 +3,7 @@ vim.api.nvim_create_user_command("Wa", ":wa", { desc = "I can't type :wa without
 vim.api.nvim_create_user_command("Wq", ":wq", { desc = "I can't type :wq without typing :Wq !" })
 vim.api.nvim_create_user_command("Wqa", ":wqa", { desc = "I can't type :wqa without typing :Waq !" })
 vim.api.nvim_create_user_command("Q", ":q", { desc = "I can't type :q without typing :Q !" })
+vim.api.nvim_create_user_command("Wall", ":wall", { desc = "I can't type :wall without typing :Wall !" })
 
 -- local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
@@ -25,3 +26,10 @@ autocmd("BufLeave", {
     pattern = "term://*",
     command = "stopinsert",
 })
+
+vim.keymap.set(
+    "n",
+    "<Leader>fj",
+    '<Cmd>%!jq --indent 4 \'def sort_keys: . as $in | if type == "object" then $in | to_entries | sort_by(.key) | from_entries | map_values(sort_keys) elif type == "array" then map(sort_keys) else . end; sort_keys\'<CR>',
+    { noremap = true, silent = true }
+)
