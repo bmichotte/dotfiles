a65c74aa37013fa3919350b23214dc32a0784d1e
Benjamin Michotte <bmichotte@gmail.com>
2024-10-02 13:55:52 +0200

    rename

---
+++ config/nvim/lua/plugins/multi-cursor.lua
@@ -0,0 +1,14 @@
+---@type LazyPlugin[]
+return {
+    {
+        "mg979/vim-visual-multi",
+        branch = "master",
+        init = function()
+            vim.g.VM_maps = {
+                ["Select Cursor Down"] = "<M-C-Down>",
+                ["Select Cursor Up"] = "<M-C-Up>",
+                ["I BS"] = "", -- disable backspace mapping to avoid an error
+            }
+        end,
+    },
+}
