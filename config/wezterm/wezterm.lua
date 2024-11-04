686d7d0c1dd2bec3689230da12e47d79e1be3c45
Benjamin Michotte <bmichotte@gmail.com>
2024-11-04 09:33:32 +0100

    use native mac window

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,17 +1,17 @@
 local wezterm = require("wezterm")
 
 local function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Latte"
-	end
+    if appearance:find("Dark") then
+        return "Catppuccin Mocha"
+    else
+        return "Catppuccin Latte"
+    end
 end
 
 local config = wezterm.config_builder()
 
-config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
+-- config.window_decorations = "RESIZE"
+-- config.enable_tab_bar = false
 config.native_macos_fullscreen_mode = true
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
@@ -21,31 +21,31 @@ config.font_size = 16.0
 config.freetype_load_target = "Light"
 
 config.keys = {
-	{
-		key = "RightArrow",
-		mods = "CMD",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		key = "LeftArrow",
-		mods = "CMD",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		key = "RightArrow",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-	{
-		key = "LeftArrow",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-	{
-		key = "Enter",
-		mods = "CMD",
-		action = wezterm.action.ToggleFullScreen,
-	},
+    {
+        key = "RightArrow",
+        mods = "CMD",
+        action = wezterm.action.ActivateTabRelative(1),
+    },
+    {
+        key = "LeftArrow",
+        mods = "CMD",
+        action = wezterm.action.ActivateTabRelative(-1),
+    },
+    {
+        key = "RightArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = "LeftArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+        key = "Enter",
+        mods = "CMD",
+        action = wezterm.action.ToggleFullScreen,
+    },
 }
 
 return config
