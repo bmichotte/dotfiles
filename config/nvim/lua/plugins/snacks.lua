e6d36a17cb916a0e7aa3d3e9dc09aeaf95f4f38a
Benjamin Michotte <bmichotte@gmail.com>
2025-01-06 08:24:27 +0100

    add Mason to keys

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -10,6 +10,48 @@ return {
             dashboard = {
                 enabled = true,
                 preset = {
+                    keys = {
+                        {
+                            icon = " ",
+                            key = "f",
+                            desc = "Find File",
+                            action = ":lua Snacks.dashboard.pick('files')",
+                        },
+                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        {
+                            icon = " ",
+                            key = "g",
+                            desc = "Find Text",
+                            action = ":lua Snacks.dashboard.pick('live_grep')",
+                        },
+                        {
+                            icon = " ",
+                            key = "r",
+                            desc = "Recent Files",
+                            action = ":lua Snacks.dashboard.pick('oldfiles')",
+                        },
+                        {
+                            icon = " ",
+                            key = "c",
+                            desc = "Config",
+                            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+                        },
+                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                        {
+                            icon = "󰒲 ",
+                            key = "L",
+                            desc = "Lazy",
+                            action = ":Lazy",
+                            enabled = package.loaded.lazy ~= nil,
+                        },
+                        {
+                            icon = " ",
+                            key = "M",
+                            desc = "Mason",
+                            action = ":Mason",
+                        },
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    },
                     header = [[
                      ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
                      ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
@@ -20,7 +62,7 @@ return {
                 },
                 sections = {
                     { section = "header" },
-                    { section = "keys", gap = 0, padding = 1, hidden = true },
+                    { section = "keys",  gap = 0, padding = 1, hidden = true },
                     {
                         icon = " ",
                         title = "Recent Files",
